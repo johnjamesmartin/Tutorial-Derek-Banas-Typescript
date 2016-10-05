cf1732f9ef5ef1dcad2cbddd1a2e5ea1de067ec1
“John <johnjamesmartin@live.com>
2016-10-05 01:28:52 +0100

    Math

---
+++ 6. Math.js
@@ -0,0 +1,16 @@
+/* Different operands in TypeScript (same as JS): */
+document.write('5 + 4 = ' + (5 + 4) + '<br/>');
+document.write('5 - 4 = ' + (5 - 4) + '<br/>');
+document.write('5 * 4 = ' + (5 * 4) + '<br/>');
+document.write('5 / 4 = ' + (5 / 4) + '<br/>');
+document.write('5 % 4 = ' + (5 % 4) + '<br/>');
+document.write('5 + String 2 = ' + (5 + '2') + '<br/>');
+/* There are different ways to increment and decrement.
+   Having ++ or -- after the variable means the current
+   value will print out and AFTER it has printed, the
+   increment or decrement will complete: */
+var randNum = 1;
+document.write('randNum++ = ' + randNum++ + '<br/>');
+document.write('++randNum = ' + ++randNum + '<br/>');
+document.write('randNum-- = ' + randNum-- + '<br/>');
+document.write('--randNum = ' + --randNum + '<br/>');
