db2e7ece409a680a9e90772051301c6a6e025a02
“John <johnjamesmartin@live.com>
2016-10-06 02:37:26 +0100

    Generic classes

--- 13. Generic Functions.js
+++ 13. Generic Functions.js
@@ -8,3 +8,21 @@ var aStr = 'A String';
 var aNum = 10;
 document.write(GetType(aStr) + '<br/>');
 document.write(GetType(aNum) + '<br/>');
+/* Generic classes: */
+var GenericNumber = (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+/* How numbers work with this class ^ */
+var aNumber = new GenericNumber();
+aNumber.add = function (x, y) {
+    return x + y;
+};
+document.write('5 + 4 = ' + aNumber.add(5, 4) + '</br>');
+/* How strings work with this class ^ */
+var aStrNum = new GenericNumber();
+aStrNum.add = function (x, y) {
+    return String(Number(x) + Number(y));
+};
+document.write('5 + 6 = ' + aStrNum.add('5', '6') + '</br>');
